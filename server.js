1bfc0a4b9bd32bf89fa9900c5fb679cb8400aee5
aifsyarifudin <96871077+aifsyarifudin@users.noreply.github.com>
2026-05-30 14:58:51 +0700

    Update server.js

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ io.on('connection', (socket) => {
                 fileName: data.fileName || '',
                 isDeleted: false,
                 seen: false,
-                createdAt: Date.now()
+                timestamp: Date.now()
             };
 
             chatHistory.push(newMessage);
@@ -110,24 +110,6 @@ io.on('connection', (socket) => {
         }
 
     });
-
-    socket.on('delete conversation', (data) => {
-
-    chatHistory = chatHistory.filter(msg => {
-
-        const chatAB =
-            msg.id === data.user1 &&
-            msg.target === data.user2;
-
-        const chatBA =
-            msg.id === data.user2 &&
-            msg.target === data.user1;
-
-        return !(chatAB || chatBA);
-
-    });
-
-});
 
     // USER KALUAR
     socket.on('disconnect', () => {
